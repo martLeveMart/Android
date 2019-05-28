434cd557e4f10898d51dfad8da09865d515c76fc
martLeveMart <mart.leveque69@gmail.com>
2019-05-28 10:20:10 +0200

    création class downloadAvis

---
+++ app/src/main/java/com/example/practice/DownloadAvis.java
@@ -0,0 +1,28 @@
+package com.example.practice;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.example.practice.model.Avis;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class DownloadAvis extends AsyncTask<URL, Integer, Avis[]> {
+
+    @Override
+    protected Avis[] doInBackground(URL... urls){
+        try{
+            publishProgress(1);
+            Avis[] result = Util.getAvis(urls[0]);
+            publishProgress(99);
+            return result;
+        }catch (IOException e){
+            return new Avis[0];
+        }
+    }
+
+    protected void onPostExecute(Avis[] avis){
+        Log.i("AVIS", "J'ai" + avis.length + " avis.");
+    }
+}
